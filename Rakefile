095a3b8d7b2fad972486c2a2dd507ac0aaaecfd0
Jonathan Rochkind <jonathan@dnil.net>
2014-03-05 15:28:39 -0500

    change to use straight .js, obtained from github release zip, with github api

--- Rakefile
+++ Rakefile
@@ -1,13 +1,9 @@
 #!/usr/bin/env rake
 require 'bundler/gem_tasks'
-require File.expand_path('../lib/chosen-rails/source_file', __FILE__)
+require File.expand_path('../lib/chosen-rails/source_fetcher', __FILE__)
 
-desc "Update with Harvest's Chosen Library"
-task 'update-chosen', 'repository_url', 'branch' do |task, args|
-  remote = args['repository_url'] || 'https://github.com/harvesthq/chosen'
-  branch = args['branch'] || 'master'
-  files = SourceFile.new
-  files.fetch remote, branch
-  files.eject_javascript_class_from_closure
-  files.cleanup
+desc "Update with Chosen Library release files from github"
+task 'update-chosen', 'github_repo', 'tag_name' do |task, args|
+  SourceFetcher.new(args['github_repo'], args['tag_name']).fetch
+  
 end
---
+++ lib/chosen-rails/source_fetcher.rb
@@ -0,0 +1,77 @@
+require 'thor'
+require 'json'
+require 'open-uri'
+
+require 'rubygems/package'
+require 'zlib'
+
+class SourceFetcher < Thor
+  include Thor::Actions
+
+  GithubRepo = 'harvesthq/chosen'
+
+  def initialize(github_repo, tag_name = nil)
+    @github_repo = github_repo || GithubRepo
+    @tag_name = tag_name
+  end
+
+  desc 'fetch source files', 'fetch source files from GitHub'
+  def fetch
+    releases_data = JSON.load( open("https://api.github.com/repos/#{@github_repo}/releases") )
+    release = if @tag_name.nil? 
+      releases_data.first
+    else
+      releases_data.find {|hash| hash["tag_name"] == @tag_name}
+    end
+
+    # We assume there's only one asset, and we want it
+    asset       = release['assets'].first
+    asset_name  = asset['name']
+
+    local_zip_path = "tmp/#{asset_name}"
+    
+    open(local_zip_path, "wb") do |file|
+      # Need to send Accept  application/octet-stream to github to get the binary
+      # http://developer.github.com/v3/repos/releases/#get-a-single-release-asset
+      file << open(asset['url'], "Accept" => "application/octet-stream").read
+    end
+
+    # We're just gonna shell out to the 'unzip' command, OSX and unix prob
+    # has it, good enough. 
+    local_source_path = "tmp/#{asset_name.chomp(File.extname(asset_name))}"
+    system("unzip", local_zip_path, "-d", local_source_path)
+
+    # Copy all the files over
+
+    
+    Dir.glob("#{local_source_path}/*.css").each do |source|
+      FileUtils.copy source, "vendor/assets/stylesheets/#{File.basename source}"
+    end
+
+    Dir.glob("#{local_source_path}/*.js").each do |source|
+      FileUtils.copy source, "vendor/assets/javascripts/#{File.basename source}"
+    end
+
+    Dir.glob("#{local_source_path}/*.{png,gif,jpg,jpeg}").each do |source|
+      FileUtils.copy source, "vendor/assets/images/#{File.basename source}"
+    end
+
+
+  end
+
+
+  protected
+  def untar(source_path)
+    
+  tar_extract = Gem::Package::TarReader.new(Zlib::GzipReader.open(source_path))
+  tar_extract.rewind # The extract has to be rewinded after every iteration
+  tar_extract.each do |entry|
+    puts entry.full_name
+    puts entry.directory?
+    puts entry.file?
+    # puts entry.read
+  end
+  tar_extract.close
+  end
+
+end
